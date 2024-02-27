56e812c6e550a9fa8f9d3a1a32bb711a7ebc81d8
Anjani Rohith Chengelli <124673886+ac185478@users.noreply.github.com>
2024-02-28 00:19:08 +0530

    initial commit

---
+++ handlers/filterLog.js
@@ -0,0 +1,43 @@
+const fs = require('fs').promises; // Import promises from file system module
+
+// Function to filter lines based on text
+function grep(lines, text) {
+  const regex = new RegExp(text, "g");
+  return lines.filter(line => regex.test(line));
+}
+
+
+// Function to perform operations from file
+async function processFile(filePath, text, outputFilePath, operation) {
+  try {
+    const data = await fs.readFile(filePath, 'utf-8');
+    const lines = data.split('\n');
+
+    let filteredLines;
+    if (operation === 'filter') {
+      filteredLines = grep(lines, text);
+    } else if (operation === 'remove') {
+      filteredLines = lines.filter(line => !line.includes(text)); // Filter again to remove lines with text
+    } else {
+      throw new Error('Invalid operation. Choose "filter" or "remove".');
+    }
+
+    await fs.writeFile(outputFilePath, filteredLines.join('\n'), 'utf-8');
+    console.log(`${operation === 'filter' ? 'Filtered' : 'Lines without'} '${text}' ${operation === 'filter' ? 'written' : 'removed'} to: ${outputFilePath}`);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Example usage
+const filePath = '/Users/AC185478/Documents/MyProjects/tracer/mock.log'; // Replace with your actual file path
+const text = ["PUBLISH","debug","mqtt-explorer"];
+const outputFilePath = 'filtered_lines.log'; // Replace with desired output file name
+const operation = ['filter','remove']; // Choose 'filter' or 'remove'
+
+// processFile(filePath, text[1], outputFilePath, operation[1]);
+processFile(filePath,text[0],outputFilePath,operation[0]);
+processFile(filePath,text[2],outputFilePath,operation[1]);
+
+
+
